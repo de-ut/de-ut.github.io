b4dc3968590c099c1d4a3c975dcb874f9d84d2dd
de-ut <balabanov_aleksei@mail.ru>
2021-05-29 00:54:29 +0300

    new version

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ $(window).ready(function(){
 var texts = $('.text-block');
 var imgs = $('.block-img');
 var stages = $('.stage');
+var spec_info = $('.spec-info .text-block');
 
 window.onscroll = function() {
     var getScr = $(document).scrollTop()+$(window).height();
     for(i = 0; i < texts.length; i++){
         if(getScr >= $(texts[i]).offset().top) $(texts[i]).css("transform", "translate(0%)");
     }
+    if(getScr >= $('.spec-info').offset().top) $(spec_info).slideDown(1000);
     for(i = 0; i < imgs.length; i++){
         if(getScr >= $(imgs[i]).offset().top) $(imgs[i]).css("transform", "translate(0%)");
     }
